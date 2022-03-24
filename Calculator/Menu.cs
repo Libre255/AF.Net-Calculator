3736cb7862552c9c86e6dc098cfa1ed9eff00e10
Libre255 <briangbv@outlook.com>
2022-03-24 21:09:41 +0100

    Add project files.

---
+++ Calculator/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CalcutatorProject
+{
+    public class Menu
+    {
+        int selectedIndex;
+        string Prompt;
+        string[] Options;
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            selectedIndex = 0;
+        }
+        private void DisplayOptions()
+        {
+            Clear();
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                WriteLine($"<< {Options[i]} >>");
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                DisplayOptions();
+                keyPressed = ReadKey(true).Key;
+
+                if(keyPressed == ConsoleKey.UpArrow)
+                {
+                    selectedIndex--;
+                    if (selectedIndex == -1)
+                    {
+                        selectedIndex = Options.Length - 1;
+                    }
+                }else if(keyPressed == ConsoleKey.DownArrow)
+                {
+                    selectedIndex++;
+                    if (selectedIndex > Options.Length - 1)
+                    {
+                        selectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+            Clear();
+            return selectedIndex;
+        }
+
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,104 @@
+﻿using static System.Console;
+using System;
+namespace CalcutatorProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calculator calculator = new Calculator();
+            calculator.menu();
+        }
+        
+    }
+    class Calculator
+    {
+        public void menu()
+        {
+            string[] methods = { "+", "-", "*", "/"};
+            Menu MainMenu = new Menu("Chose the mathematic method", methods);
+            int seletedMethod = MainMenu.Run();
+
+            switch (seletedMethod)
+            {
+                case 0: Sum();
+                    break;
+                case 1: Minus();
+                    break;
+                case 2: Multiplication();
+                    break;
+                case 3:Division();
+                    break;
+
+            }
+        }
+        private int[] instructions(string method)
+        {
+            WriteLine("Write the first number then press enter");
+            WriteLine("Then Write the second number and press enter");
+            int n1 = NrValidator();
+            WriteLine(method);
+            int n2 = NrValidator();
+            WriteLine($"-----");
+            return new int[]{n1, n2};
+        }
+        private void ReRunMenu()
+        {
+            WriteLine(" ");
+            WriteLine("Press any key to go back to the menu");
+            ReadKey();
+            menu();
+        }
+        private void Sum()
+        {
+            int[] numbers = instructions("+");
+            WriteLine($"{numbers[0] + numbers[1]}");
+            ReRunMenu();
+        }
+
+        private void Minus()
+        {
+            int[]numbers = instructions("-");
+            WriteLine($"{numbers[0] - numbers[1]}");
+            ReRunMenu();
+        }
+        private void Multiplication()
+        {
+            int[] numbers = instructions("*");
+            WriteLine($"{numbers[0] * numbers[1]}");
+            ReRunMenu();
+        }
+        private void Division()
+        {
+            int[] numbers = instructions("/");
+            if(numbers[0] == 0 || numbers[1] == 0)
+            {
+                WriteLine("0");
+                WriteLine("you trying to divide by zero");
+            }
+            else
+            {
+                WriteLine($"{numbers[0]/numbers[1]}");
+            }
+            
+            ReRunMenu();
+        }
+
+        private int NrValidator()
+        {
+            int checkRuns = 1;
+            int numberOutput;
+            string input;
+            do
+            {
+                if (checkRuns >= 2)
+                {
+                    WriteLine("You can only write numbers, please try again");
+                }
+                input = ReadLine();
+                checkRuns++;
+            } while (int.TryParse(input, out numberOutput) == false);
+            return numberOutput;
+        }
+    }
+}
